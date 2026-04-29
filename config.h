02701adf27c07e75e224a9a4990fabdc372e4c65
Skullheadx <admonty1@protonmail.com>
2026-04-29 05:08:35 -0400

    add currently playing song

--- config.h
+++ config.h
@@ -68,6 +68,7 @@ static const struct arg args[] = {
 	/* function format          argument */
 	{ username, " %s@",  NULL },
 	{ hostname, "%s |",  NULL },
+	{ run_command, " %s |", "mpc"},
 	{ cpu_perc, " CPU %s%% |",  NULL },
 	{ ram_perc, " RAM %s%% |",  NULL },
 /*	{ swap_perc, " SWAP %s%% |",  NULL },*/
